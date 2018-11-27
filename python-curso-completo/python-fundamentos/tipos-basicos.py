276df0905796213708731d9478e586441edc36a7
robsonoduarte <robson.o.d@gmail.com>
2018-11-27 17:39:59 -0200

    add aula sobre tipos básicos

---
+++ python-curso-completo/python-fundamentos/tipos-basicos.py
@@ -0,0 +1,26 @@
+# boolean 
+a = True
+b = False
+
+print(a,b)
+
+# int, float
+a = 1
+b = 1.2
+
+print(a,b)
+
+# string
+a = 'string com aspas simples'
+b = "string com aspas duplas"
+print(a,b)
+
+# list
+a = [1,2,3]
+print(a)
+
+
+#None
+a = None
+print(a)
+
