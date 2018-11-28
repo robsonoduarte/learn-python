b4bcc4ed6688f9d7b00f4d97f3c2b26d837f9141
robsonoduarte <robson.o.d@gmail.com>
2018-11-28 19:42:34 -0200

    add o desafio da aula.

--- python-curso-completo/python-fundamentos/operadores-aritmeticos.py
+++ python-curso-completo/python-fundamentos/operadores-aritmeticos.py
@@ -5,3 +5,16 @@ print(9.4 / 3) # divisão
 print(9.4 // 3) # divisão com resultado em inteiro
 print( 2 ** 2) # potenciação
 print( 2 % 2) # módulo
+
+#calculando a porcentagem utilizando somente operadores aritmeticos
+salario = 100
+despesas = 45
+percentualDespesas = despesas * 100 / salario
+
+print(percentualDespesas) 
+
+
+
+
+
+
