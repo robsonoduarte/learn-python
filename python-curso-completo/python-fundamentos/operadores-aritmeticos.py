c0414cb08a6922a8aff92d6d3b99fd42a9420d53
robsonoduarte <robson.o.d@gmail.com>
2018-11-28 19:34:30 -0200

    operadores aritmeticos

---
+++ python-curso-completo/python-fundamentos/operadores-aritmeticos.py
@@ -0,0 +1,7 @@
+print(1 + 2) # soma
+print(3 - 1) # subtração
+print(3 * 3.2) # multiplicação
+print(9.4 / 3) # divisão
+print(9.4 // 3) # divisão com resultado em inteiro
+print( 2 ** 2) # potenciação
+print( 2 % 2) # módulo
