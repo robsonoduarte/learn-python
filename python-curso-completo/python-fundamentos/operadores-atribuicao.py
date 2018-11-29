6cad803e97982f4022d0d0128bbf20998cd4c8c2
robsonoduarte <robson.o.d@gmail.com>
2018-11-29 21:43:20 -0200

    finalizando a aula de operadores de atribuição.

--- python-curso-completo/python-fundamentos/operadores-atribuicao.py
+++ python-curso-completo/python-fundamentos/operadores-atribuicao.py
@@ -1,5 +1,27 @@
-'''
-Created on 29 de nov de 2018
+a = 3 # atribuição simples
+print(a)
 
-@author: robso
-'''
+a = a + 7 # atribuição com uma operação
+print(a)
+
+a += 5 # atribuição aditiva = a = a + 5
+print(a)
+
+a -= 3 # atribuição subtrativa
+print(a)
+
+a *= 2 # atribuição multiplicativa
+print(a)
+
+a /= 4 # atribuição divisiva
+print(a)
+
+
+a %= 4 # atribuição modular
+print(a)
+
+a **= 8 # atribuição com potenciação
+print(a)
+
+a //= 255 # atribuição com divisão inteira
+print(a)
