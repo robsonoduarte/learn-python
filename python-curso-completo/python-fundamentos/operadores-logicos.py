f459227b9e4ce0eb1b5911f4561c03cdb1597fa6
robsonoduarte <robson.o.d@gmail.com>
2018-11-30 19:46:58 -0200

    operadores lógicos.

---
+++ python-curso-completo/python-fundamentos/operadores-logicos.py
@@ -0,0 +1,26 @@
+# Operador AND
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+
+# Operador OR
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
+
+
+# Operador XOR
+print(True != True)
+print(True != False)
+print(False != True)
+print(False != False)
+
+
+# Operadore de Negação
+print(not True)
+print(not False)
+print(not 0)
+print(not 1)
+print(not not 1)
