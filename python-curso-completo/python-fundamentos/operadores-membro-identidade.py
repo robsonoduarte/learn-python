75049d380e363e817c6f973d25bef34254648be5
robsonoduarte <robson.o.d@gmail.com>
2018-12-04 18:06:18 -0200

    operadores Membro/Identidade.

---
+++ python-curso-completo/python-fundamentos/operadores-membro-identidade.py
@@ -0,0 +1,19 @@
+# Operador de Membro
+
+lista = [1, 2, 3, 'Ana', 'Robson']
+
+print(2 in lista)
+print('Ana' not in lista)
+
+
+# Operador de Identidade
+
+x = 3
+y = x
+z = 3
+print(x is y)
+print(y is x)
+print(z is x)
+print(z is not x)
+
+
