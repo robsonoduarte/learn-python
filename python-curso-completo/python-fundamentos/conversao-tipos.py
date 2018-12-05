6ad28f6397bcb8c140756d2e19fc377f61bd66af
robsonoduarte <robson.o.d@gmail.com>
2018-12-05 20:52:59 -0200

    conversão de tipos.

---
+++ python-curso-completo/python-fundamentos/conversao-tipos.py
@@ -0,0 +1,12 @@
+from builtins import str
+a = 3
+b = '2'
+
+print(type(a))
+print(type(b))
+
+print(type(str(a))) # int para string
+print(type(int(b))) # string para int
+
+# mais funções de conversão => https://docs.python.org/3/library/functions.html
+
