186b969feeb93f1b08b4838c4d189f81f0ca2eb3
robsonoduarte <robson.o.d@gmail.com>
2018-12-12 17:05:53 -0200

    finalizando aula de numeros.

--- python-curso-completo/python-fundamentos/numeros.py
+++ python-curso-completo/python-fundamentos/numeros.py
@@ -12,7 +12,12 @@ from decimal import Decimal, getcontext
 
 print(Decimal(1) / Decimal(7)) # usando o Decimal para aumentar a precisão
 
-getcontext().prec = 4 # usando o Decimal para aumentar a precisão
+getcontext().prec = 4 # ajustando a precisão do Decimal 
 
 print(Decimal(1) / Decimal(7)) 
+
+print(1.1 + 2.2) # não utiliza a precisão ajustada do decimal 
+print(Decimal(1.1) + Decimal(2.2))
+
+
 
