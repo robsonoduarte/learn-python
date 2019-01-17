23646c58b96b667bd6506b72424fe39a64f51d25
robsonoduarte <robson.o.d@gmail.com>
2019-01-16 22:02:18 -0200

    aula 1 de string

---
+++ python-curso-completo/python-fundamentos/strings.py
@@ -0,0 +1,19 @@
+dir(str)
+nome = 'Robson Duarte'
+nome[0]
+# nome[0] = 'P' String são imutaveis
+
+"Marca d' água" == 'Marca d\' água' #podemos "escapar" a string ou utilizar "
+
+texto = 'Texto entre apostrófos pode ter "apasa"'
+
+doc = """Texto com múltiplas 
+...linhas"""
+print(doc)
+
+print('Texto com múltipla...\n linhas')
+
+doc = '''Também é possível... 
+com 3 aspas simples'''
+print(doc)
+
