c7fbc3ae5ebf796d809363e96f24edf9bdfd9027
robsonoduarte <robson.o.d@gmail.com>
2019-01-22 21:48:35 -0200

    aula 3 de lista.

--- python-curso-completo/python-fundamentos/listas.py
+++ python-curso-completo/python-fundamentos/listas.py
@@ -1,9 +1,12 @@
+
+# criando e adicionando
 lista = []
 lista.append(1)
 lista.append(2)
 lista.append([2,3,4])
 lista
 
+# criando e removendo e revertendo
 lista = [1, 2, "Robson", "Ana"]
 lista
 lista.remove(2)
@@ -11,8 +14,17 @@ lista
 lista.reverse()
 lista
 
+# acessando elementos
 lista = [1, 4, "Antonio", "Aparecida", 3.1415]
 lista.index("Aparecida")
-1 in lista
 lista[4]
 lista[-1]
+
+# acessando elementos com range
+lista = ["Ana", "Lia", "Rui", "Paulo", "Dani"]
+lista[1:3]
+lista[1:-1]
+lista[1:]
+lista[:]
+lista[::2]
+lista[::-1]
