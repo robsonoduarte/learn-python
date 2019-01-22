15c71ec7809df32a932b263869a1e1a954e7161e
robsonoduarte <robson.o.d@gmail.com>
2019-01-22 21:57:16 -0200

    aula de tupla.

---
+++ python-curso-completo/python-fundamentos/tuplas.py
@@ -0,0 +1,15 @@
+tupla = tuple()
+tupla
+tupla = ()
+tupla
+tupla = ('um',) # tupla de 1 elemento
+tupla[0]
+
+tupla = ('verde', 'amarelo', 'azul', 'branco')
+tupla[1]
+tupla[-1]
+tupla.index("verde")
+tupla.count("azul")
+
+
+
