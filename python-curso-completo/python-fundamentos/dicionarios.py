354f8e95466ec14691c5dc2330d1665d817ace03
robsonoduarte <robson.o.d@gmail.com>
2019-01-22 22:07:06 -0200

    aula 1 de dicionarios.

---
+++ python-curso-completo/python-fundamentos/dicionarios.py
@@ -0,0 +1,13 @@
+pessoa = {'nome': 'Robson', 'idade': 42, 'cursos': ['Java', 'Python']}
+pessoa
+
+pessoa['nome']
+pessoa['idade']
+pessoa['cursos']
+pessoa['cursos'][0]
+
+pessoa.keys()
+pessoa.values()
+pessoa.items()
+pessoa.get('idade')
+pessoa.get('tags', '[]')
