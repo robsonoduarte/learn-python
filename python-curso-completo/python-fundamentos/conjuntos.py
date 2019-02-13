863f1bbdfe531c9564e2ee0405cff47c4adb38e2
robsonoduarte <robson.o.d@gmail.com>
2019-02-12 22:26:57 -0200

    finalizando a aula de conjuntos.

--- python-curso-completo/python-fundamentos/conjuntos.py
+++ python-curso-completo/python-fundamentos/conjuntos.py
@@ -1,7 +1,26 @@
-values = [1, 2, 4]
-valuesToCheck = [1, 2, 4.1]
+a = {1,2,3}
+type(a)
 
-for i, value in enumerate(values):    
-    if value < valuesToCheck[i]:
-          print("maior")
-          
+a = set("robson")          
+print(a)
+
+print( '3' in a , 4 not in a)
+
+{1,2,3} == {3,2,1,3}
+
+# operacoes
+c1 = {1,2}
+c2 = {2,3}
+c1.union(c2)
+c1.intersection(c2)
+c1.update(c2)
+print(c1)
+
+c2 <= c1
+c1 >= c2
+
+{1,2,3} - {2}
+c1 - c2
+
+c1 -= {2}
+c1
