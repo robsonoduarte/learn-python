c4c98677b4b221c5d2904708f3fd4b71659d31e0
robsonoduarte <robson.o.d@gmail.com>
2019-02-13 07:38:18 -0200

    finalizando aula de interpolação.

--- python-curso-completo/python-fundamentos/interpolacao.py
+++ python-curso-completo/python-fundamentos/interpolacao.py
@@ -1,9 +1,9 @@
-# Comentário de uma linha
+from string import Template
 
-'''
-Comentário de multiplas linhas com aspas simples
-'''
+nome, idade = 'Ana', 30.9875
+print('Nome: %s Idade %.2f' % (nome,idade)) # mais antiga
+print('Nome: {0} Idade: {1}').format(nome,idade)
+print(f'Nome: {nome} Idade: {idade}') #>= 3.6
 
-"""
-Comentário de multiplas linhas com aspas duplas
-"""
+s = Template('Nome $nome Idade: $idade')
+print(s.substitute(nome=nome, idade=idade))
