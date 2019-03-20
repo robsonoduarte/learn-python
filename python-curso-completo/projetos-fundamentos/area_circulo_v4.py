d950d35089f88cebcf991b9758562f0ba7b4f7c9
robsonoduarte <robson.o.d@gmail.com>
2019-03-19 21:53:55 -0300

    exemplo de funcao

--- python-curso-completo/projetos-fundamentos/area_circulo_v4.py
+++ python-curso-completo/projetos-fundamentos/area_circulo_v4.py
@@ -1,10 +1,10 @@
 from math import pi
 
+
+def circulo(raio):
+    print("Área do círculo => ", pi * float(raio) ** 2)
+
+
 if __name__ == '__main__':
-    raio = input("Informe o raio:")
-    area = pi * float(raio) ** 2
-    print(area)
-
-print('Nome do módulo', __name__)
-print('Nome do pacote', __package__)
-
+    raio = input("Informe o raio: ")
+    circulo(raio)
