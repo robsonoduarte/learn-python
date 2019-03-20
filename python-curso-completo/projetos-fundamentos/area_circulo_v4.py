694ef614e773eb9ad765130ee29657d2d31a3939
robsonoduarte <robson.o.d@gmail.com>
2019-03-19 21:45:47 -0300

    add if para rodar somente no modulo main

--- python-curso-completo/projetos-fundamentos/area_circulo_v4.py
+++ python-curso-completo/projetos-fundamentos/area_circulo_v4.py
@@ -1,10 +1,9 @@
 from math import pi
 
-raio = input("Informe o raio:")
-
-area = pi * float(raio) ** 2
-
-print(area)
+if __name__ == '__main__':
+    raio = input("Informe o raio:")
+    area = pi * float(raio) ** 2
+    print(area)
 
 print('Nome do módulo', __name__)
 print('Nome do pacote', __package__)
