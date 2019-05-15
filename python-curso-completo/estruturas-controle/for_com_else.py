736119e1e4e23c130b2e111b50e5830fbd15ac44
robsonoduarte <robson.o.d@gmail.com>
2019-05-14 22:20:57 -0300

    aula for com else

---
+++ python-curso-completo/estruturas-controle/for_com_else.py
@@ -0,0 +1,14 @@
+PALAVRAS_PROIBIDAS = ('futebol', 'religião', 'futebol')
+
+textos = [
+    'Pedro gosta de futebol e política',
+    "A praia foi divertida",
+]
+
+for texto in textos:
+    for palavra in texto.lower().split():
+        if palavra in PALAVRAS_PROIBIDAS:
+            print("Texto possui pelo menos um palavra proibida:", palavra)
+            break
+    else:
+        print("Texto autorizado:", texto)
