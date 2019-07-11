30c9055705e9dac97e91c1da1d4a831650064aa2
robsonoduarte <robson.duarte@grupozap.com>
2019-07-11 18:45:44 -0300

    aula 1

---
+++ dojo-python-grupozap/aula-1/example_1.py
@@ -0,0 +1,15 @@
+from requests import get
+from requests import post as bravox #usando elias
+#from requests import * nao recomendado importar tudo
+#from requests nao recomendado importar implicito
+
+
+def func():
+    print('bravox')
+    v1 = 1 # atribuindo em um variavel
+    v2 = None # null no python
+    v3 = 2 + 2 # operacao matematica
+
+
+def main():
+    func()
