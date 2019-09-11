d1a60c419a28074f6e5b6181d801b40cafdc6518
Robson Duarte <robson.o.d@gmail.com>
2019-09-10 22:40:20 -0300

    dictionary

---
+++ python-curso-completo/list_comprehension/comprehension_v5.py
@@ -0,0 +1,9 @@
+# {expr form item in list if conditional}
+
+dictionary = {i: i* 2 for i in range(10) if i % 2 == 0}
+print(dictionary)
+
+
+for number, double in dictionary.items():
+    print(f'{number} x 2 = {double}')
+
