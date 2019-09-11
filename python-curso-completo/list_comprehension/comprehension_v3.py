dbf9b12ce5c300020088c1ca0d7998a881215a3f
Robson Duarte <robson.o.d@gmail.com>
2019-09-10 22:25:46 -0300

    generator

---
+++ python-curso-completo/list_comprehension/comprehension_v3.py
@@ -0,0 +1,9 @@
+# (expr form item in list if conditional)
+
+generator = (i * 2 for i in range(10) if i % 2 == 0)
+print(next(generator))
+print(next(generator))
+print(next(generator))
+print(next(generator))
+
+
