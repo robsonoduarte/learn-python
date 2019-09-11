bbe6d3d7a3d2a500e1b63f71bc8d8bbd2d3293e2
Robson Duarte <robson.o.d@gmail.com>
2019-09-10 22:30:15 -0300

    list generator in for

--- python-curso-completo/list_comprehension/comprehension_v4.py
+++ python-curso-completo/list_comprehension/comprehension_v4.py
@@ -1,9 +1,8 @@
 # (expr form item in list if conditional)
 
 generator = (i ** 2 for i in range(10) if i % 2 == 0)
-print(next(generator))
-print(next(generator))
-print(next(generator))
-print(next(generator))
 
+
+for number in generator:
+    print(number)
 
