a0d3586233e3fb23ef134a20906c5375c4043b65
Robson Duarte <robson.o.d@gmail.com>
2020-03-30 20:00:41 -0300

    danilo help

---
+++ danilo/aula2-entrada-e-saida.py
@@ -0,0 +1,11 @@
+#dica já se acostume a usar os nomes, etc... tudo em ingles 
+#name
+#age...
+nome =  input('Escreva seu nome: ')
+idade = 26
+altura = 1.80
+
+print(nome, 'tem', idade, 'anos e', altura, 'de altura')
+
+#dica use String Interpolation https://www.python.org/dev/peps/pep-0498/
+print(f"{nome} tem {idade} anos e {altura} de altura")
